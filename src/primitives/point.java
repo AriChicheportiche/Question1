9e5f6de1f19227d6ab68fee370d78e8bfec12a22
Aribo <63457208+AriChicheportiche@users.noreply.github.com>
2023-02-27 19:26:23 +0200

    first dummy

---
+++ src/primitives/point.java
@@ -0,0 +1,55 @@
+package primitives;
+
+import java.util.Objects;
+
+public class point {
+    double x;
+    double y;
+    double z;
+
+    public point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        point point = (point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0 && Double.compare(point.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+
+
+}
